7f1ca8dcf8795ce877efd262922a926c5546847f
emily <moira.vanderslagmolen@ulb.be>
2023-05-02 11:29:52 +0200

    sql script

--- sqlDB.sql
+++ sqlDB.sql
@@ -2,25 +2,54 @@
 -- Drop table
 -- DROP TABLE public."Médecin";
 CREATE TABLE public."Médecin" (
-    totoid int4 NULL,
+    medecin varchar(50) NULL,
+    inami int4 NULL,
+    mail varchar(50) NULL,
+    nom varchar(50) NULL,
+    specialite varchar(50) NULL,
+    telephone int4 NULL
+);
+-- public.médicament definition
+-- Drop table
+-- DROP TABLE public.médicament;
+CREATE TABLE public.médicament (
     dci varchar(50) NULL,
     "nom Commercial" varchar(50) NULL,
     "système anatomique" varchar(50) NULL,
-    conditionnement int4 NULL,
+    conditionnement int4 NULL
+);
+-- public.pathologie definition
+-- Drop table
+-- DROP TABLE public.pathologie;
+CREATE TABLE public.pathologie (
+    "Dépendance à la caféine" varchar(50) NULL,
+    addictologie varchar(50) NULL
+);
+-- public.patient definition
+-- Drop table
+-- DROP TABLE public.patient;
+CREATE TABLE public.patient (
     niss_patient int4 NULL,
     medecin varchar(50) NULL,
     inami_medecin int4 NULL,
     pharmacien varchar(50) NULL,
     inami_pharmacien int4 NULL,
     medicament_nom_commercial varchar(50) NULL,
+    dci varchar(50) NULL,
     date_prescription varchar(50) NULL,
     date_vente varchar(50) NULL,
     duree_traitement int4 NULL
 );
--- public.pathologie definition
+-- public.pharmacien definition
 -- Drop table
--- DROP TABLE public.pathologie;
-CREATE TABLE public.pathologie ();
+-- DROP TABLE public.pharmacien;
+CREATE TABLE public.pharmacien (
+    pharmacien varchar(50) NULL,
+    inami int4 NULL,
+    mail varchar(50) NULL,
+    nom varchar(50) NULL,
+    tel int4 NULL,
+);
 -- public.prescription definition
 -- Drop table
 -- DROP TABLE public.prescription;
@@ -29,15 +58,3 @@ CREATE TABLE public.prescription ();
 -- Drop table
 -- DROP TABLE public.traitement;
 CREATE TABLE public.traitement ();
--- public.médicament definition
--- Drop table
--- DROP TABLE public.médicament;
-CREATE TABLE public.médicament ();
--- public.pharmacien definition
--- Drop table
--- DROP TABLE public.pharmacien;
-CREATE TABLE public.pharmacien ();
--- public.patient definition
--- Drop table
--- DROP TABLE public.patient;
-CREATE TABLE public.patient ();
