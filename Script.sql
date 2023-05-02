9e1977742212bfc9f5317ae7a6f70b9f9feb8443
emily <moira.vanderslagmolen@ulb.be>
2023-05-02 13:23:46 +0200

    [CLEAN] + [ADD] foreign keys

--- Script.sql
+++
@@ -1,2 +0,0 @@
-create table toto
-(totoid int);
--- dossier_medical.sql
+++ dossier_medical.sql
@@ -9,7 +9,9 @@ CREATE TABLE Patient (
     Adresse_mail VARCHAR(255) NOT NULL,
     Numero_de_telephone VARCHAR(20) NOT NULL,
     Numero_INAMI_medecin INT,
-    Numero_INAMI_pharmacien INT
+    Numero_INAMI_pharmacien INT,
+    FOREIGN KEY (Numero_INAMI_medecin) REFERENCES Medecin(Numero_INAMI),
+    FOREIGN KEY (Numero_INAMI_pharmacien) REFERENCES Pharmacien(Numero_INAMI),
 );
 
 CREATE TABLE Medecin (
@@ -18,7 +20,16 @@ CREATE TABLE Medecin (
     Specialite VARCHAR(255) NOT NULL,
     Systeme_anatomique VARCHAR(255) NOT NULL,
     Numero_de_telephone VARCHAR(20) NOT NULL,
-    Adresse_mail VARCHAR(255) NOT NULL
+    Adresse_mail VARCHAR(255) NOT NULL,
+    FOREIGN KEY (Specialite) REFERENCES Specialite(nom),
+);
+
+-- public.specialites definition
+CREATE TABLE Specialite (
+    nom varchar PRIMARY KEY,
+    medicament1 varchar NULL,
+    medicament2 varchar NULL,
+    medicament3 varchar NULL,
 );
 
 CREATE TABLE Pharmacien (
@@ -35,12 +46,16 @@ CREATE TABLE Pathologie (
     Date_de_diagnostic DATE NOT NULL,
     NISS INT,
     FOREIGN KEY (NISS) REFERENCES Patient(NISS)
+    FOREIGN KEY (Systeme_anatomique) REFERENCES Specialite(nom)
 );
 
 CREATE TABLE Medicament (
     Nom_DCI VARCHAR(255) PRIMARY KEY,
     Nom_commercial VARCHAR(255) NOT NULL,
-    Conditionnement VARCHAR(255) NOT NULL
+    Conditionnement VARCHAR(255) NOT NULL,
+    Systeme_Anatomique varchar(50) NULL
+    FOREIGN KEY (Systeme_anatomique) REFERENCES Specialite(nom)
+
 );
 
 CREATE TABLE Prescription (
@@ -55,14 +70,18 @@ CREATE TABLE Prescription (
     FOREIGN KEY (Nom_DCI) REFERENCES Medicament(Nom_DCI)
 );
 
+-- dossier patient dans le data
 CREATE TABLE Traitement (
+    ID_Traitement INT PRIMARY KEY,
     Date_de_debut DATE NOT NULL,
+    date_vente varchar(50) NULL,
     Duree INT NOT NULL,
     NISS INT,
     Numero_INAMI INT,
     Nom_DCI VARCHAR(255),
     ID_Prescription INT,
     ID_Pathologie INT,
+
     FOREIGN KEY (NISS) REFERENCES Patient(NISS),
     FOREIGN KEY (Numero_INAMI) REFERENCES Medecin(Numero_INAMI),
     FOREIGN KEY (Nom_DCI) REFERENCES Medicament(Nom_DCI),
