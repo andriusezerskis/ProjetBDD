e240bee16f01e4e410cd2bd9f326a3f3462f1371
Matias <74540729+matias1419@users.noreply.github.com>
2023-05-02 15:24:05 +0200

    modifi .sql + add import et test import de patient

--- dossier_medical.sql
+++ dossier_medical.sql
@@ -1,90 +1,79 @@
 CREATE DATABASE dossier_medical;
 
-CREATE TABLE Patient (
-    NISS INT PRIMARY KEY,
-    Nom VARCHAR(255) NOT NULL,
-    Prenom VARCHAR(255) NOT NULL,
-    Gendre VARCHAR(50) NOT NULL,
-    Date_de_naissance DATE NOT NULL,
-    Adresse_mail VARCHAR(255) NOT NULL,
-    Numero_de_telephone VARCHAR(20) NOT NULL,
-    Numero_INAMI_medecin INT,
-    Numero_INAMI_pharmacien INT,
-    FOREIGN KEY (Numero_INAMI_medecin) REFERENCES Medecin(Numero_INAMI),
-    FOREIGN KEY (Numero_INAMI_pharmacien) REFERENCES Pharmacien(Numero_INAMI),
+-- Création de la table 'patient'
+CREATE TABLE patient (
+    NISS VARCHAR(15) PRIMARY KEY,
+    nom VARCHAR(50) NOT NULL,
+    prenom VARCHAR(50) NOT NULL,
+    genre INT NOT NULL,
+    date_de_naissance DATE NOT NULL,
+    mail VARCHAR(100),
+    telephone VARCHAR(15),
+    inami_medecin VARCHAR(15),
+    inami_pharmacien VARCHAR(15)
 );
 
-CREATE TABLE Medecin (
-    Numero_INAMI INT PRIMARY KEY,
-    Nom VARCHAR(255) NOT NULL,
-    Specialite VARCHAR(255) NOT NULL,
-    Systeme_anatomique VARCHAR(255) NOT NULL,
-    Numero_de_telephone VARCHAR(20) NOT NULL,
-    Adresse_mail VARCHAR(255) NOT NULL,
-    FOREIGN KEY (Specialite) REFERENCES Specialite(nom),
+-- Création de la table 'medecin'
+CREATE TABLE medecin (
+    inami VARCHAR(15) PRIMARY KEY,
+    nom VARCHAR(50) NOT NULL,
+    mail VARCHAR(100),
+    specialite VARCHAR(50) NOT NULL,
+    telephone VARCHAR(15)
 );
 
--- public.specialites definition
-CREATE TABLE Specialite (
-    nom varchar PRIMARY KEY,
-    medicament1 varchar NULL,
-    medicament2 varchar NULL,
-    medicament3 varchar NULL,
+-- Création de la table 'pharmacien'
+CREATE TABLE pharmacien (
+    inami VARCHAR(15) PRIMARY KEY,
+    nom VARCHAR(50) NOT NULL,
+    mail VARCHAR(100),
+    telephone VARCHAR(15)
 );
 
-CREATE TABLE Pharmacien (
-    Numero_INAMI INT PRIMARY KEY,
-    Nom VARCHAR(255) NOT NULL,
-    Numero_de_telephone VARCHAR(20) NOT NULL,
-    Adresse_mail VARCHAR(255) NOT NULL
+-- Création de la table 'pathologie'
+CREATE TABLE pathologie (
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL,
+    systeme_anatomique VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE Pathologie (
-    ID_Pathologie INT PRIMARY KEY,
-    Nom VARCHAR(255) NOT NULL,
-    Systeme_anatomique VARCHAR(255) NOT NULL,
-    Date_de_diagnostic DATE NOT NULL,
-    NISS INT,
-    FOREIGN KEY (NISS) REFERENCES Patient(NISS)
-    FOREIGN KEY (Systeme_anatomique) REFERENCES Specialite(nom)
+-- Création de la table 'medicament'
+CREATE TABLE medicament (
+    id SERIAL PRIMARY KEY,
+    dci VARCHAR(100) NOT NULL,
+    nom_commercial VARCHAR(100) NOT NULL,
+    systeme_anatomique VARCHAR(100) NOT NULL,
+    conditionnement INT NOT NULL
 );
 
-CREATE TABLE Medicament (
-    Nom_DCI VARCHAR(255) PRIMARY KEY,
-    Nom_commercial VARCHAR(255) NOT NULL,
-    Conditionnement VARCHAR(255) NOT NULL,
-    Systeme_Anatomique varchar(50) NULL
-    FOREIGN KEY (Systeme_anatomique) REFERENCES Specialite(nom)
-
+-- Création de la table 'diagnostic'
+CREATE TABLE diagnostic (
+    id SERIAL PRIMARY KEY,
+    NISS_patient VARCHAR(15) NOT NULL,
+    date_diagnostic DATE NOT NULL,
+    pathologie_id INT NOT NULL,
+    FOREIGN KEY (NISS_patient) REFERENCES patient(NISS),
+    FOREIGN KEY (pathologie_id) REFERENCES pathologie(id)
 );
 
-CREATE TABLE Prescription (
-    ID_Prescription INT PRIMARY KEY,
-    Date_de_prescription DATE NOT NULL,
-    Duree_de_traitement INT NOT NULL,
-    NISS INT,
-    Numero_INAMI INT,
-    Nom_DCI VARCHAR(255),
-    FOREIGN KEY (NISS) REFERENCES Patient(NISS),
-    FOREIGN KEY (Numero_INAMI) REFERENCES Medecin(Numero_INAMI),
-    FOREIGN KEY (Nom_DCI) REFERENCES Medicament(Nom_DCI)
+-- Création de la table 'prescription'
+CREATE TABLE prescription (
+    id SERIAL PRIMARY KEY,
+    inami_medecin VARCHAR(15) NOT NULL,
+    inami_pharmacien VARCHAR(15) NOT NULL,
+    medicament_id INT NOT NULL,
+    NISS_patient VARCHAR(15) NOT NULL,
+    date_prescription DATE NOT NULL,
+    duree_traitement INT NOT NULL,
+    FOREIGN KEY (inami_medecin) REFERENCES medecin(inami),
+    FOREIGN KEY (inami_pharmacien) REFERENCES pharmacien(inami),
+    FOREIGN KEY (medicament_id) REFERENCES medicament(id),
+    FOREIGN KEY (NISS_patient) REFERENCES patient(NISS)
 );
 
--- dossier patient dans le data
-CREATE TABLE Traitement (
-    ID_Traitement INT PRIMARY KEY,
-    Date_de_debut DATE NOT NULL,
-    date_vente varchar(50) NULL,
-    Duree INT NOT NULL,
-    NISS INT,
-    Numero_INAMI INT,
-    Nom_DCI VARCHAR(255),
-    ID_Prescription INT,
-    ID_Pathologie INT,
-
-    FOREIGN KEY (NISS) REFERENCES Patient(NISS),
-    FOREIGN KEY (Numero_INAMI) REFERENCES Medecin(Numero_INAMI),
-    FOREIGN KEY (Nom_DCI) REFERENCES Medicament(Nom_DCI),
-    FOREIGN KEY (ID_Prescription) REFERENCES Prescription(ID_Prescription),
-    FOREIGN KEY (ID_Pathologie) REFERENCES Pathologie(ID_Pathologie)
+-- Création de la table 'specialite'
+CREATE TABLE specialite (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    medicament_systeme_anatomique VARCHAR(100) NOT NULL
 );
