586a1f0198a3b634d0f0ac2a22c91af2a6171557
matias <matiasnieto1419@gmail.com>
2023-05-15 18:47:29 +0200

    fix class Database

--- MVC/model.py
+++ MVC/model.py
@@ -1,121 +1,138 @@
 import psycopg2
+from psycopg2 import sql, Error
 
 class Database:
     def __init__(self):
-        self.conn = psycopg2.connect(host='localhost', user='postgres', password='azertyuiop', dbname='dossier_medical')
+        self.db_config = {
+            'host':'localhost',
+            'user':'postgres',
+            'password':'azertyuiop',
+            'dbname':'dossier_medical'
+        }
+
+    def _connect(self):
+        return psycopg2.connect(**self.db_config)
 
     def add_patient(self, niss, nom, prenom, genre, date_de_naissance, mail, telephone, inami_medecin, inami_pharmacien):
-        with self.conn.cursor() as cursor:
-            # Vérifie si un patient avec le même NISS existe déjà
-            cursor.execute("SELECT NISS FROM patient WHERE NISS = %s", (niss,))
-            if cursor.fetchone():
-                return False  # Le patient existe déjà
-            
-            sql = """INSERT INTO patient (NISS, nom, prenom, genre, date_de_naissance, mail, telephone, inami_medecin, inami_pharmacien)
-                     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-            cursor.execute(sql, (niss, nom, prenom, genre, date_de_naissance, mail, telephone, inami_medecin, inami_pharmacien))
-            self.conn.commit()
-            return True  # Le patient a été ajouté avec succès
+        with self._connect() as conn:
+            with conn.cursor() as cursor:
+                try:
+                    sql = """INSERT INTO patient (NISS, nom, prenom, genre, date_de_naissance, mail, telephone, inami_medecin, inami_pharmacien)
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+                    cursor.execute(sql, (niss, nom, prenom, genre, date_de_naissance, mail, telephone, inami_medecin, inami_pharmacien))
+                    conn.commit()
+                    return True
+                except Error:
+                    return False
 
     def add_medecin(self, inami, nom, mail, specialite, telephone):
-        with self.conn.cursor() as cursor:
-            # Vérifie si un médecin avec le même numéro INAMI existe déjà
-            cursor.execute("SELECT inami FROM medecin WHERE inami = %s", (inami,))
-            if cursor.fetchone():
-                return False  # Le médecin existe déjà
+        with self._connect() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("SELECT id_specialite FROM specialite WHERE nom = %s", (specialite,))
+                result = cursor.fetchone()
 
-            sql = """INSERT INTO medecin (inami, nom, mail, specialite, telephone)
-                    VALUES (%s, %s, %s, %s, %s)"""
-            cursor.execute(sql, (inami, nom, mail, specialite, telephone))
-            self.conn.commit()
-            return True  # Le médecin a été ajouté avec succès
+                if result is None:
+                    print(f"La spécialité {specialite} n'existe pas.")
+                    return False
+
+                id_specialite = result[0]
+
+                try:
+                    sql = """INSERT INTO medecin (inami, nom, mail, id_specialite, telephone)
+                            VALUES (%s, %s, %s, %s, %s)"""
+                    cursor.execute(sql, (inami, nom, mail, id_specialite, telephone))
+                    conn.commit()
+                    return True
+                except Error :
+                    return False
 
 
     def add_pharmacien(self, inami, nom, mail, telephone):
-        with self.conn.cursor() as cursor:
-            # Vérifie si un pharmacien avec le même numéro INAMI existe déjà
-            cursor.execute("SELECT inami FROM pharmacien WHERE inami = %s", (inami,))
-            if cursor.fetchone():
-                return False  # Le pharmacien existe déjà
-            
-            sql = """INSERT INTO pharmacien (inami, nom, mail, telephone)
-                     VALUES (%s, %s, %s, %s)"""
-            cursor.execute(sql, (inami, nom, mail, telephone))
-            self.conn.commit()
-            return True  # Le pharmacien a été ajouté avec succès
-    
-    def get_patient(self, niss, date_de_naissance):
-        sql = "SELECT * FROM patient WHERE NISS = %s AND date_de_naissance = %s"
-        cursor = self.conn.cursor()
-        
-        cursor.execute(sql, (niss, date_de_naissance))
-        result = cursor.fetchone()
-        if result:
-            keys = ["NISS", "nom", "prenom", "genre", "date_de_naissance", "mail", "telephone", "inami_medecin", "inami_pharmacien"]
-            return dict(zip(keys, result))
-        return None
-        
+        with self._connect() as conn:
+            with conn.cursor() as cursor:
+                try:
+                    sql = """INSERT INTO pharmacien (inami, nom, mail, telephone)
+                            VALUES (%s, %s, %s, %s)"""
+                    cursor.execute(sql, (inami, nom, mail, telephone))
+                    conn.commit()
+                    return True
+                except Error :
+                    return False
 
+    def get_patient(self, niss, date_de_naissance):
+        with self._connect() as conn:
+            with conn.cursor() as cursor:
+                sql = "SELECT * FROM patient WHERE NISS = %s AND date_de_naissance = %s"
+                cursor.execute(sql, (niss, date_de_naissance))
+                result = cursor.fetchone()
+                if result:
+                    keys = ["NISS", "nom", "prenom", "genre", "date_de_naissance", "mail", "telephone", "inami_medecin", "inami_pharmacien"]
+                    return dict(zip(keys, result))
+                return None
 
     def update_patient_medecin(self, niss, inami_medecin):
-        print(niss)
-        print(inami_medecin)
-        with self.conn.cursor() as cursor:
-            sql = """UPDATE patient SET inami_medecin = %s WHERE NISS = %s"""
-            cursor.execute(sql, (inami_medecin, niss))
-            self.conn.commit()
+        with self._connect() as conn:
+            with conn.cursor() as cursor:
+                sql = """UPDATE patient SET inami_medecin = %s WHERE NISS = %s"""
+                cursor.execute(sql, (inami_medecin, niss))
+                conn.commit()
 
     def update_patient_pharmacien(self, niss, inami_pharmacien):
-        with self.conn.cursor() as cursor:
-            sql = """UPDATE patient SET inami_pharmacien = %s WHERE NISS = %s"""
-            cursor.execute(sql, (inami_pharmacien, niss))
-            self.conn.commit()
-    
+        with self._connect() as conn:
+            with conn.cursor() as cursor:
+                sql = """UPDATE patient SET inami_pharmacien = %s WHERE NISS = %s"""
+                cursor.execute(sql, (inami_pharmacien, niss))
+                conn.commit()
 
     def get_medical_info(self, NISS_patient):
-        with self.conn.cursor() as cursor:
-            cursor.execute("""
-                    SELECT
-                        diagnostic.date_diagnostic,
-                        pathologie.nom AS nom_pathologie
-                    FROM diagnostic
-                    JOIN pathologie ON diagnostic.pathologie_id = pathologie.id
-                    WHERE diagnostic.NISS_patient = %s
-                """, (NISS_patient,))
-            diagnostics_info = cursor .fetchall()
+        with self._connect() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("""
+                        SELECT
+                            diagnostique.date_diagnostic,
+                            pathologie.nom AS nom_pathologie
+                        FROM diagnostique
+                        JOIN pathologie ON diagnostique.id_pathologie = pathologie.id_pathologie
+                        WHERE diagnostique.NISS_patient = %s
+                    """, (NISS_patient,))
+                diagnostics_info = cursor.fetchall()
         
         return diagnostics_info
     
     def get_traitements(self, NISS_patient):
-        with self.conn.cursor() as cursor:
-            cursor.execute("""
-                SELECT
-                    prescription.date_vente,
-                    medicament.nom_commercial,
-                    prescription.duree_traitement
-                FROM prescription
-                JOIN medicament ON prescription.medicament_id = medicament.id
-                WHERE prescription.NISS_patient = %s;
-            """, (NISS_patient,))
-            traitements_info = cursor.fetchall()
+        with self._connect() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("""
+                    SELECT
+                        prescription.date_vente,
+                        medicament_conditionnement.nom_commercial,
+                        prescription.duree_traitement
+                    FROM prescription
+                    JOIN medicament ON prescription.id_medicament = medicament.id_medicament
+                    JOIN medicament_conditionnement ON medicament.id_medicament = medicament_conditionnement.id_medicament
+                    WHERE prescription.NISS_patient = %s;
+                """, (NISS_patient,))
+                traitements_info = cursor.fetchall()
         
         return traitements_info
     
     def get_contact_ref(self, NISS_patient):
-        with self.conn.cursor() as cursor:
-            cursor.execute("""
-                SELECT
-                    medecin.nom,
-                    medecin.mail,
-                    medecin.telephone,
-                    pharmacien.nom,
-                    pharmacien.mail,
-                    pharmacien.telephone
-                FROM patient
-                JOIN medecin ON patient.inami_medecin = medecin.inami
-                JOIN pharmacien ON patient.inami_pharmacien = pharmacien.inami
-                WHERE patient.NISS = %s;
-            """ , (NISS_patient,))
-            contact_info = cursor.fetchone()
-        
+        with self._connect() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("""
+                    SELECT
+                        medecin.nom,
+                        medecin.mail,
+                        medecin.telephone,
+                        pharmacien.nom,
+                        pharmacien.mail,
+                        pharmacien.telephone
+                    FROM patient
+                    JOIN medecin ON patient.inami_medecin = medecin.inami
+                    JOIN pharmacien ON patient.inami_pharmacien = pharmacien.inami
+                    WHERE patient.NISS = %s;
+                """ , (NISS_patient,))
+                contact_info = cursor.fetchone()
+
         return contact_info
+
