acb24b7289b8830b2dd4f8833c2ceb8239cdc4d9
matias <matiasnieto1419@gmail.com>
2023-05-23 21:34:48 +0200

    r1

--- requete/requete1.sql
+++ requete/requete1.sql
@@ -1,4 +1,5 @@
-SELECT dci, nom_commercial, conditionnement
-FROM medicament
-WHERE dci = 'YOUR_DCI'
-ORDER BY nom_commercial ASC, conditionnement ASC;
+SELECT dci, nom_commercial, conditionnement -- On sélectionne les colonnes qui nous intéressent
+FROM medicament -- On sélectionne la table qui nous intéresse
+WHERE dci = 'YOUR_DCI' -- On filtre sur la DCI qui nous intéresse
+ORDER BY nom_commercial ASC, conditionnement ASC; -- On trie par nom commercial puis par conditionnement
+
