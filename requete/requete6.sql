87cc8752f85bdfa4c4bfe1faf6ab62ddb0a51f46
matias <matiasnieto1419@gmail.com>
2023-05-26 11:23:26 +0200

    commanitaire r6

--- requete/requete6.sql
+++ requete/requete6.sql
@@ -1,14 +1,12 @@
-SELECT
-  medecin.inami AS medecin_inami,
-  medecin.nom AS medecin_nom,
+SELECT medecin.inami AS medecin_inami, medecin.nom AS medecin_nom, -- On sélectionne les colonnes qui nous intéressent
   STRING_AGG(DISTINCT medicament.nom_commercial, ', ') AS medicament_noms
-FROM prescription
-INNER JOIN medecin ON prescription.inami_medecin = medecin.inami
-INNER JOIN medicament ON prescription.id_medicament = medicament.id_medicament
-WHERE NOT EXISTS (
-  SELECT NULL 
+FROM prescription -- On sélectionne les tables qui nous intéressent
+INNER JOIN medecin ON prescription.inami_medecin = medecin.inami -- On joint la table medecin
+INNER JOIN medicament ON prescription.id_medicament = medicament.id_medicament -- On joint la table medicament
+WHERE NOT EXISTS ( -- On fait une sous-requête pour exclure les médecins qui prescrivent des médicaments de leur spécialité
+  SELECT NULL
   FROM specialite_systeme_anatomique ssa 
-  WHERE ssa.id_systeme_anatomique = medicament.id_systeme_anatomique 
+  WHERE ssa.id_systeme_anatomique = medicament.id_systeme_anatomique
   AND ssa.id_specialite = medecin.id_specialite
 )
 GROUP BY medecin.inami, medecin.nom;
